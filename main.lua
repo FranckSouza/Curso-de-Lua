a7fb64fa5bbb92c78884aafab5fa80b38a9ad2c7
FranckSouza <francksouzah@gmail.com>
2023-07-16 14:39:08 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,55 @@
+--[[
+
+Curso : Linguagem Lua 
+Dev: Franck Souza
+Prof: Elinaldo santos YTB
+Data: 16 /07 / 2023
+Fonte: https://www.youtube.com/@elinaldosantosdegoesjunior2987  |   https://www.lua.org/
+]]
+
+
+
+
 -- comentario em linha
+
+--[[
+este  é um comentário em bloco
+]]
+
+
+
+--Variáveis em lua  conceito basico: 
+
+--são impotante para que possamos armazenar, ou seja, atribuir valores nelas, e o nome de cada variável deve respeitar algumas regras  ex.: 
+
+a = 40 
+b = 23 
+c = 3.5
+
+a = " texto"
+a = 'texto'
+
+
+var = false
+var1 = true
+
+var2 = nil
+
+--na linguagem Lua tem uma tipagem dinâmica, ou seja, cada uma das variáveis que iremos criar não precisa dizer qual é o tipo,
+--ela por si só ja atribui o tipo de acordo com o valor inserido 
+
+
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--Operadores Aritméticos básicos em Lua   ex.:
+
+a = 4+7  - operador   soma
+b = 3-4  - operador Subtração
+c = 4/2  - operador Divisão 
+d = 8*0  - operador Multiplicação 
+e = 7%2  - operador  Resto da divisão 
+f = 2^3  - operador Exponenciação 
+
+
+
